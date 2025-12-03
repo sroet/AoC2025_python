a78bceebc05ae784ecca887c0e3c425adcd9b591
Sander Roet <sanderroet@hotmail.com>
2025-12-03 10:39:14 +0100

    combine code for both parts

--- 03/solution.py
+++ 03/solution.py
@@ -11,37 +11,12 @@ def read_file(fname):
                 data.append([int(i) for i in strip])
     return data
 
-
-def part_1(data):
-    out = 0
-    for row in data:
-        tens = -1
-        idx_10 = 0
-        while idx_10 < len(row) - 1:
-            tens = row[idx_10]
-            ones = -1
-            idx = idx_10 + 1
-            while idx < len(row):
-                i = row[idx]
-                if i > tens and idx < len(row) - 1:
-                    idx_10 = idx
-                    break
-                if i > ones:
-                    ones = i
-                idx += 1
-            else:
-                break
-        # print(f"{tens=} {ones=}")
-        out += 10 * tens + ones
-    return out
-
-
-def part_2(data):
+def parts(data, digits):
     out = 0
     for row in data:
         temp_out = 0
         min_idx = 0
-        for i in range(11, -1, -1):
+        for i in range(digits-1, -1, -1):
             if i == 0:
                 s = slice(min_idx, None)
             else:
@@ -60,11 +35,11 @@ def part_2(data):
 def main(fname):
     start = time.time()
     data = read_file(fname)
-    total_1 = part_1(data)
+    total_1 = parts(data, 2)
     t1 = time.time()
     print(f"Part 1: {total_1}")
     print(f"Ran in {t1-start} s")
-    total_2 = part_2(data)
+    total_2 = parts(data, 12)
     print(f"Part 2: {total_2}")
     print(f"Ran in {time.time()-t1} s")
     print(f"Total ran in {time.time()-start} s")
